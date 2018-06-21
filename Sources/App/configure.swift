1e132df95738957f274e9ad4125a0cbe6e61694f
Peterxor <peter55605@gmail.com>
2018-06-21 09:55:23 +0800

    delete Leaf

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,14 +1,10 @@
 import Vapor
-import Leaf
 import FluentSQLite
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
-    
-    try services.register(LeafProvider())
-    config.prefer(LeafRenderer.self, for: ViewRenderer.self)
     
     try services.register(FluentSQLiteProvider())
     var databases = DatabasesConfig()
